ebe8b8c144f0896e9cfac71592f9aac45ed98ddc
Kakadu <Kakadu@pm.me>
2023-10-17 17:04:59 +0300

    Garbage collection by Kakadu
    Signed-off-by: Azatic <gabrahmanovaza@gmail.com>

--- my_tests/a.ml
+++ my_tests/a.ml
@@ -1,13 +1,18 @@
 let rec fac n = if n <=1 then 1 else n * fac(n-1)
 
 external popcount : int->int->int -> int = "myfunc"
-external check: string -> int = "caml_check"
-external c_fac: int -> int = "caml_fac"
+(*external check: string -> int = "caml_check"
+external c_fac: int -> int = "caml_fac" *)
+
+let () = print_int (popcount 3 2 1)
+(*
 let () =  
 	 (* print_int(fac 5);
     print_int(check "hello");
 	print_newline(); *) 
-	Printf.printf "%d\n" (popcount 3 2 2);
+	Printf.printf "%d\n" (popcount 3 2 1);
 (*	 print_newline(); 
 	print_int(c_fac 5);
     print_float(sqrt 4.); *)
+*)
+
