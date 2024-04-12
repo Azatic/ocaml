e4e424cbcadf7414bd966a4531f335aa2ada403b
Azatic <gabrahmanovaza@gmail.com>
2024-04-12 11:17:46 +0000

    add simple scalar vector len 4 sum

--- my_tests/default_vec.ml
+++ my_tests/default_vec.ml
@@ -1,6 +1,7 @@
 let vec_add l1 l2 =
-  let sum = Array.make 2 0 in
-  for i = 0 to 1 do
-    sum.(i) <- l1.(i) + l2.(i)
-  done;
+  let sum = Array.make 4 0 in
+  sum.(0) <- l1.(0) + l2.(0);
+  sum.(1) <- l1.(1) + l2.(1); 
+  sum.(2) <- l1.(2) + l2.(2);
+  sum.(3) <- l1.(3) + l2.(3);
   sum
--- my_tests/vec.ml
+++ my_tests/vec.ml
@@ -1,7 +1,11 @@
 
-external shiftadd : int array ->int array->int -> int array = "vec_add"
+external vecadd : int array ->int array -> int array = "vec_add"
 
 let arr1 = [| 1; 2; 3; 4 |]
 let arr2 = [| 1; 2; 3; 4 |]
 
-let arr3 = (shiftadd arr1 arr2 4) 
+let arr3 = vecadd arr1 arr2 
+
+
+  let () = print_int arr3.(0);
+
