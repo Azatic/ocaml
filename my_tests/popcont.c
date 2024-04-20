18afd5440591e467d3c834e00233d400b4b7f9ad
Azatic <gabrahmanovaza@gmail.com>
2024-04-20 12:49:40 +0000

    right vector load command

--- my_tests/popcont.c
+++ my_tests/popcont.c
@@ -1,23 +1,18 @@
-#include <caml/mlvalues.h>
-#include <caml/memory.h>
+//#include <caml/mlvalues.h>
+//#include <caml/memory.h>
 
-CAMLprim value popcount(value arg)
-{
-	CAMLparam1(arg);
-
-	CAMLreturn(Val_int(__builtin_popcount(Int_val(arg))));
-}
+//}
 int vec_add(int n, int e)
 {
 	return 1;
 }
 
-CAMLprim value caml_check(value arg)
-{
-		CAMLparam1(arg);
+//CAMLprim value caml_check(value arg)
+//{
+//		CAMLparam1(arg);
 
-		CAMLreturn(Val_int(12));
-}
+//		CAMLreturn(Val_int(12));
+//}
 int factorial(int n)  
 {
   if (n == 0)  
@@ -26,9 +21,9 @@ int factorial(int n)
     return(n * factorial(n-1));  
 }
 
-CAMLprim value caml_fac(value n)
-{
-		CAMLparam1(n);
+//CAMLprim value caml_fac(value n)
+//{
+//		CAMLparam1(n);
 
-		CAMLreturn(Val_int(factorial(Int_val(n))));
-}
+//		CAMLreturn(Val_int(factorial(Int_val(n))));
+//}  
--- my_tests/vec.ml
+++ my_tests/vec.ml
@@ -1,8 +1,8 @@
 
 external vecadd : int array ->int array -> int array = "vec_add"
 
-let arr1 = [| 1; 2; 3; 4;3;4 |]
-let arr21 = [| 2; 10; 2; 2;3;1;110|]
+let arr1 = [| 111; 2; 3; 4;3;4 |]
+let arr21 = [| 211; 10; 2; 2;3;1;110|]
 
 
 
@@ -11,5 +11,5 @@ let arr3 = vecadd arr1 arr21
 
 
 
-let () = print_int arr3.(0)
+(*let () = print_int arr3.(0)*)
 let () = Array.iter (fun x -> print_int x; print_string " ") arr3;
