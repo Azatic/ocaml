7acf74f0418943653bca9a482e973003fafe6f98
Azatic <gabrahmanovaza@gmail.com>
2024-04-23 08:26:28 +0000

    use right tmp registers

--- my_tests/vec.ml
+++ my_tests/vec.ml
@@ -11,13 +11,13 @@ let ar2 = [|2|]
 (*let vecadd1 arr1 arr2 = vecadd arr1 arr2 arr2.len*)
 (*let arr4 = vector_add ar1 ar2 1*)
 let arr3 = vector_add arr1 arr21 8
-let arr4 = vector_add ar1 ar2 1
+(*let arr4 = vector_add ar1 ar2 1*)
 (*let i = find ar1 1
 (*let arr4 = vector_add ar1 ar2 1*)
 let () = print_int i*)
 (*let () = print_int arr3.(0)*)
 let () = Array.iter (fun x -> print_int x; print_string " ") arr3
-let () = Array.iter (fun x -> print_int x; print_string " ") arr4
+let () = Array.iter (fun x -> print_int x; print_string " ") arr3
 
 
 
